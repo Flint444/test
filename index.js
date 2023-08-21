6c4f2df9eedbb1f09846024d79027f7127ba6ba0
Flint444 <maksimgrabauskas@gmail.com>
2023-08-21 14:22:50 +0200

    Fourth commit

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 console.log("Sum 1 + 2 =", 1+2)
 console.log("Sum 3 + 4 =", 3+4)
 console.log("Sum 5 + 4 =", 5+4)
+console.log("Sum 5 + 4 =", 5+4)
+console.log("Sum 5 + 4 =", 5+4)
+console.log("Sum 5 + 4 =", 5+4)
+console.log("Sum 5 + 4 =", 5+4)
+console.log("Sum 5 + 4 =", 5+4)
